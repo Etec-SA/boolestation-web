bece20b9808a029772db3768975b1765132b63e4
iamthepoe <luca1901p@gmail.com>
2023-10-18 19:59:59 -0300

    fix: image optimization

--- src/pages/atoms/Banner.tsx
+++ src/pages/atoms/Banner.tsx
@@ -1,15 +1,21 @@
+import Image from 'next/image'
+import logo from '../../../public/DarkLogo.png'
+
 const Banner = () => {
   return (
     <>
-      <img
-        src="https://raw.githubusercontent.com/Etec-SA/diagrams/main/logos/DarkLogo.png"
-        height="485"
-        width="650"
-        alt="BooleStation Logo"
-      />
-      <h2 className="text-white">
-        Para pensar melhor, domine as leis do pensamento.
-      </h2>
+      <div className="flex flex-col justify-center items-center w-full">
+        <Image
+          src={logo}
+          width={1080}
+          height={323}
+          alt="BooleStation Logo"
+          priority
+        />
+        <h2 className="text-white">
+          Para pensar melhor, domine as leis do pensamento.
+        </h2>
+      </div>
     </>
   )
 }
--- src/pages/atoms/BoolestationIntroduction.tsx
+++ src/pages/atoms/BoolestationIntroduction.tsx
@@ -3,25 +3,24 @@ import ExtLink from '../../components/ext-link'
 
 const BoolestationIntroduction = () => {
   return (
-    <div className="flex justify-around items-center h-[80vh] w-full max-sm:h-[40vh] max-sm:justify-center max-sm:text-center bg-[rgba(0,0,0,0.2)] my-10">
+    <div className="flex justify-around items-center h-[80vh] w-full max-sm:h-[40vh] max-sm:justify-center max-sm:text-center bg-[rgba(0,0,0,0.2)] max-sm:bg-inherit my-10">
       <div className="p-6 w-7/12 h-64 max-sm:w-full max-sm:h-32">
         <TypeAnimation
           sequence={[
-            // Same substring at the start will only be typed out once, initially
             'Boolestation: o que é?',
-            1500, // wait 1s before replacing "Mice" with "Hamsters"
+            1500,
             'Boolestation: onde encontrar?',
             1500,
             'Boolestation: como participar?',
             1500,
           ]}
-          wrapper="h4"
+          wrapper="span"
           speed={50}
           className="text-2xl text-lime-400 max-sm:text-sm max-md:text-lg max-xl:text-3xl max-2xl:text-4xl"
           repeat={Infinity}
         />
         <br />
-        <p className="max-sm:text-xs max-md:text-sm text-md">
+        <p className="max-sm:text-xs max-md:text-sm text-md my-8">
           Boolestation se trata de uma{' '}
           <strong>plataforma para o aprendizado de lógica.</strong> Idealizado
           em 2023 por cinco estudantes da <b>Etec de Peruíbe</b>, o projeto visa
